1e64f75760b32bfed0b640e8db686d4c7ff9c6f0
Soria <jsoriaf2400@alumno.ipn.mx>
2025-10-21 09:43:19 -0600

    Inicio de clase de caracteres

---
+++ Char/src/Cadena.java
@@ -0,0 +1,22 @@
+public class Cadena{
+    public static void main(String[] args){
+        char a1 = 'H';
+        char a2 = 'o';
+        char a3 = 'l';
+        char a4 = 97;
+        char a5 = '\n';
+        //Imprime caracter
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        //Imprime valor Unicode/Ascii
+        System.out.println((int)(a1));
+        System.out.println((int)(a2));
+        System.out.println((int)(a3));
+        System.out.println((int)(a4));
+        System.out.println((int)(a5));
+
+    }
+}
